40576d17964a22c7c16e4183375076a7691f0843
JUANDATM <kira00711@hotmail.com>
2020-01-27 11:53:25 -0600

    Examen Terminado Ok

--- app.js
+++ app.js
@@ -1,18 +1,58 @@
-"use strict";
-//let avenger ={
-//  nombre:"Steve",
-//clave:"Capitan America",
-//poder:"Droga"
-//}
-function enviarMision(xmen) {
-    console.log('Enviando a:' + xmen.nombre);
-}
-function enviarCuartel(xmen) {
-    console.log('Enviando al cuartel a:' + xmen.nombre);
-}
-var wolverine = {
-    nombre: "Wolwerine",
-    poder: "Regeneracion"
+// Uso de Let y Const
+var nombre = "Ricardo Tapia";
+var edad = 23;
+var PERSONAJE = {
+    nombre: nombre,
+    edad: edad
 };
-enviarMision(wolverine);
-enviarCuartel(wolverine);
+console.log(PERSONAJE);
+function imprimir(batman) {
+    console.log(batman.nombre + " sabe estas artes marciales :" + batman.artesMarciales);
+}
+var batman = {
+    nombre: "Bruno Díaz",
+    artesMarciales: ["Karate", "Aikido", "Wing Chun", "Jiu-Jitsu"]
+};
+imprimir(batman);
+// Convertir esta funcion a una funcion de flecha
+var resultadoDoble = function (a, b) { return ((a + b) * 2); };
+console.log(resultadoDoble(4, 8));
+// Función con parametros obligatorios, opcionales y por defecto
+// donde NOMBRE = obligatorio
+//       PODER  = opcional
+//       ARMA   = por defecto = "arco"
+function getAvenger(nombre, poder, arma) {
+    if (arma === void 0) { arma = "arco"; }
+    var mensaje;
+    if (poder) {
+        mensaje = nombre + " tiene el poder de: " + poder + " y un arma: " + arma;
+    }
+    else {
+        mensaje = nombre + " tiene un " + poder;
+    }
+    return mensaje;
+}
+;
+console.log(getAvenger("JUAN", "Invisibilidad"));
+// Cree una clase que permita manejar la siguiente estructura
+// La clase se debe de llamar rectangulo,
+// debe de tener dos propiedades:
+//   * base
+//   * altura
+// También un método que calcule el área  =  base * altura,
+// ese método debe de retornar un numero. 
+var rectangulo = /** @class */ (function () {
+    function rectangulo() {
+        this.base = 0;
+        this.altura = 0;
+    }
+    return rectangulo;
+}());
+var area = new rectangulo();
+function getArea(base, altura) {
+    area.base = base;
+    area.altura = altura;
+    var areaRec = area.altura * area.base;
+    return areaRec;
+}
+console.log(getArea(8, 5));
--- app.ts
+++ app.ts
@@ -1,43 +1,71 @@
 
-//let avenger ={
-  //  nombre:"Steve",
-    //clave:"Capitan America",
-    //poder:"Droga"
-//}
 
-//let {poder,nombre,clave}=avenger;
-
-////console.log(nombre,clave,poder);
-/*--------------------------------------------------------------------*/
-
-////let avengers:string[]=["Thor","Steve","Tony"];
-/////let [thor,capitan,tony]=avengers;
-//////console.log(thor, capitan, tony);
-/*---------------------------------------------------------------------------- */
-
-
-
-
-
-
-
-
-
-
-interface Xmen{
-    nombre:string,
-    poder:string 
-}
-
-function enviarMision(xmen :{nombre:string}){
-console.log('Enviando a:'+xmen.nombre)
-}
-function enviarCuartel(xmen :{nombre:string}){
-    console.log('Enviando al cuartel a:'+xmen.nombre)
-    }
-let wolverine = {
-    nombre:"Wolwerine",
-    poder:"Regeneracion"
+// Uso de Let y Const
+const nombre = "Ricardo Tapia";
+const edad = 23;
+let PERSONAJE = {
+  nombre: nombre,
+  edad: edad
 };
-enviarMision(wolverine);
-enviarCuartel(wolverine);
+console.log(PERSONAJE);
+
+// Cree una interfaz que sirva para validar el siguiente objeto
+interface Batman{
+    nombre:string,
+    artesMarciales:string[]
+}
+
+function imprimir(batman : Batman) {
+    console.log(batman.nombre + " sabe estas artes marciales :"+ batman.artesMarciales);
+   
+}
+let batman:Batman= {
+    nombre: "Bruno Díaz",
+    artesMarciales: ["Karate","Aikido","Wing Chun","Jiu-Jitsu"]
+  }
+
+ imprimir(batman);
+
+// Convertir esta funcion a una funcion de flecha
+let resultadoDoble = (a:number, b:number ) => ((a+ b) * 2);
+
+
+console.log(resultadoDoble(4,8)); 
+ 
+// Función con parametros obligatorios, opcionales y por defecto
+// donde NOMBRE = obligatorio
+//       PODER  = opcional
+//       ARMA   = por defecto = "arco"
+function getAvenger( nombre:string, poder?:string, arma= "arco" ){
+  var mensaje;
+  if( poder ){
+     mensaje = nombre + " tiene el poder de: " + poder + " y un arma: " + arma;
+  }else{
+     mensaje = nombre + " tiene un " + poder
+  }
+  return mensaje;
+};
+console.log(getAvenger("JUAN","Invisibilidad"));
+
+// Cree una clase que permita manejar la siguiente estructura
+// La clase se debe de llamar rectangulo,
+// debe de tener dos propiedades:
+//   * base
+//   * altura
+// También un método que calcule el área  =  base * altura,
+// ese método debe de retornar un numero. 
+
+class rectangulo{
+    base:number=0;
+    altura:number=0;
+}
+let area:rectangulo = new rectangulo();
+
+function getArea(base:any,altura:any){
+    area.base=base;
+    area.altura=altura;
+let areaRec=area.altura*area.base;
+return areaRec;
+}
+
+console.log(getArea(8,5));
